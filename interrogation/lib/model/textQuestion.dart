ac5e99f409650d677162253cd0be51272c6197a3
andrey.molochko <andrey.molochko@exposit.com>
2019-04-08 06:01:03 -0700

    create test question

--- interrogation/lib/model/textQuestion.dart
+++ interrogation/lib/model/textQuestion.dart
@@ -1,1 +1,19 @@
-import '';
+import 'question.dart';
+
+class TextQuestion extends Question{
+  String _textAnswer;
+
+  TextQuestion(String name, String type, List<String> answers) : super(name, type,
+      answers);
+
+  TextQuestion.withId(int id, String name, String type, List<String> answers) : super
+    .withId(id, name, type, answers);
+
+  String get textAnswer => _textAnswer;
+
+  set textAnswer(String value) {
+    _textAnswer = value;
+  }
+
+
+}
